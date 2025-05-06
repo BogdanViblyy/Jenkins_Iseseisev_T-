fe4c43ba673d31e17b8efe00bd0b6c8bc13ab749
BogdanViblyy <153904857+BogdanViblyy@users.noreply.github.com>
2025-05-06 17:26:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Моё любимое сладкое блюдо — тирамису!');
+    res.send('Моё любимое сладкое блюдо — шоколадный эклер!');
 });
 
 app.listen(port, () => {
